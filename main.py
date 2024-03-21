fix sign
Signed-off-by: HyuninLee <hyunin@wifi-10-44-81-222.wifi.berkeley.edu>

main.py
@@ -106,10 +106,10 @@ def VR_PDPG(env,agent,previous_agent,agent_reference,args,num_states,num_actions
             set_flat_grads_to(agent,d_L)
             for g in optimizer_agent.param_groups:
                 g['lr'] = lr_theta
-            optimizer_agent.step()
+            optimizer_agent.step() # gradient step
 
             ## line 7
-            mu = mu - init_lr_mu * 0.5 * torch.norm(occupancy_measure_gap)**2
+            mu = mu + init_lr_mu * (0.5 * torch.norm(occupancy_measure_gap) ** 2 - 10) #g(\lambda)
             mu = torch.clamp(mu, min=0, max=args.C0_mu)
 
             ## define some vairbales
@@ -189,7 +189,7 @@ def VR_PDPG(env,agent,previous_agent,agent_reference,args,num_states,num_actions
             optimizer_agent.step()
 
             ## line 14
-            mu = mu - init_lr_mu * 0.5 * torch.norm(occupancy_measure_gap) ** 2
+            mu = mu + init_lr_mu * (0.5 * torch.norm(occupancy_measure_gap) ** 2 - 10) #g(\lambda)
             mu = torch.clamp(mu, min=0, max=C0_mu)
 
             ## define some vairbales

utils.py
@@ -53,7 +53,9 @@ def get_gradient_input_reward(reward,gamma,obs_buffer,action_buffer,agent_,optim
     else :
         _, batch_logprob = agent_.get_action(batch_input)
         selected_logprob = batch_logprob.gather(1, action_buffer[episode, :].long().view(-1, 1))
-    loss = -torch.sum(selected_logprob * scores)
+    # this could be problem!! #
+    loss = - torch.sum(selected_logprob * scores)
+    ##########################
     loss.backward()
     d = get_flat_grads_from(agent_)
 