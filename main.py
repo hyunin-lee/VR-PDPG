fix error- w function / r_g sign
Signed-off-by: HyuninLee <hyunin@wifi-10-44-81-222.wifi.berkeley.edu>

main.py
@@ -19,14 +19,15 @@ import numpy as np
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--max_episode", type=int, default=10, help = "iteration number")
+    parser.add_argument("--max_episode", type=int, default=1000, help = "iteration number")
     parser.add_argument("--max_step", type=int, default = 1000, help = "trajectory length")
-    parser.add_argument("--gamma", type=float, default=0.99, help="gamma")
+    parser.add_argument("--gamma", type=float, default=0.85, help="gamma")
     parser.add_argument("--init_lr_theta", type=float, default=0.001, help="initial learning rate for theta")
     parser.add_argument("--init_lr_mu", type=float, default=0.001, help="initial learning rate for mu")
     parser.add_argument("--alpha", type=float, default=0.1, help="alpha")
-    parser.add_argument("--init_mu", type=float, default=0.1, help="initial mu")
-    parser.add_argument("--C0_mu", type=float, default=100, help="alpha")
+    parser.add_argument("--init_mu", type=float, default=2, help="initial mu")
+    parser.add_argument("--C0_mu", type=float, default=10, help="alpha")
+    parser.add_argument("--d_0", type=float, default=2, help="violance allowance")
     args = parser.parse_args()
     return args
 
@@ -40,6 +41,7 @@ def VR_PDPG(env,agent,previous_agent,agent_reference,args,num_states,num_actions
     alpha = args.alpha
     init_mu = args.init_mu
     C0_mu = args.C0_mu
+    d_0 = args.d_0
 
     # define optimizer.zero_grad()
     optimizer_agent = torch.optim.Adam(agent.parameters(), lr=init_lr_theta)
@@ -89,7 +91,7 @@ def VR_PDPG(env,agent,previous_agent,agent_reference,args,num_states,num_actions
             # line 4
             r_f = reward_buffer[episode]
             occupancy_measure_gap = occupancy_measure - target_occupancy_measure
-            r_g = compress_reward(obs_buffer[episode,:],action_buffer[episode,:],num_actions,occupancy_measure_gap)
+            r_g = - compress_reward(obs_buffer[episode,:],action_buffer[episode,:],num_actions,occupancy_measure_gap) #donghao
 
             # line 5
             ## compute r_f ##
@@ -109,7 +111,7 @@ def VR_PDPG(env,agent,previous_agent,agent_reference,args,num_states,num_actions
             optimizer_agent.step() # gradient step
 
             ## line 7
-            mu = mu + init_lr_mu * (0.5 * torch.norm(occupancy_measure_gap) ** 2 - 20) #g(\lambda)
+            mu = mu + init_lr_mu * (0.5 * torch.norm(occupancy_measure_gap) ** 2 - d_0) #g(\lambda)
             mu = torch.clamp(mu, min=0, max=args.C0_mu)
 
             ## define some vairbales
@@ -151,7 +153,7 @@ def VR_PDPG(env,agent,previous_agent,agent_reference,args,num_states,num_actions
             # line 11
             r_f = reward_buffer[episode]
             occupancy_measure_gap = lambda_ - target_occupancy_measure
-            r_g = compress_reward(obs_buffer[episode,:],action_buffer[episode,:],num_actions,occupancy_measure_gap)
+            r_g = - compress_reward(obs_buffer[episode,:],action_buffer[episode,:],num_actions,occupancy_measure_gap) #donghao
 
             # line 12
             ## compute d_f ##
@@ -189,7 +191,7 @@ def VR_PDPG(env,agent,previous_agent,agent_reference,args,num_states,num_actions
             optimizer_agent.step()
 
             ## line 14
-            mu = mu + init_lr_mu * (0.5 * torch.norm(occupancy_measure_gap) ** 2 - 20) #g(\lambda)
+            mu = mu + init_lr_mu * (0.5 * torch.norm(occupancy_measure_gap) ** 2 - d_0) #g(\lambda)
             mu = torch.clamp(mu, min=0, max=C0_mu)
 
             ## define some vairbales
@@ -207,6 +209,7 @@ def VR_PDPG(env,agent,previous_agent,agent_reference,args,num_states,num_actions
         writer.add_scalar("return", torch.sum(reward_buffer[episode]).item(), episode)
         writer.add_scalar("final step", final_step, episode)
         writer.add_scalar("constraint violation", torch.sum(occupancy_measure - target_occupancy_measure).item(), episode)
+        print(torch.sum(occupancy_measure - target_occupancy_measure).item())
         # print("reward : " + str(torch.sum(reward_buffer[episode]).item()))
         # print("final step : " + str(final_step))
         # print("constraint violation : " + str(torch.sum(occupancy_measure - target_occupancy_measure).item()))

utils.py
@@ -37,25 +37,23 @@ class Agent(nn.Module):
 
         if action is None:
             action = probs.sample()
-        log_probs = probs.log_prob(action)
-        if return_prob:
-            return action, torch.log(probs.probs)
-        else:
-            return action, torch.log(probs.probs)
+        # log_probs = probs.log_prob(action)
+
+        return action, torch.log(probs.probs)
 
 def get_gradient_input_reward(reward,gamma,obs_buffer,action_buffer,agent_,optimizer_,episode,num_states,requires_grad=True):
     optimizer_.zero_grad()
-    scores = calculate_scores(reward, gamma)
+    scores = calculate_scores(reward, gamma) # computes f(h) = \sum_{k=h}^{H-1} \gamma^k r(s_k,a_k)
+    # scores is a $H$ length - vector with [f(0),f(1),...,f(H-1)]
     batch_input = F.one_hot(obs_buffer[episode, :].type(torch.long), num_classes=num_states).type(torch.float)
     if requires_grad :
         _, batch_logprob = agent_.get_action(Variable(batch_input))
-        selected_logprob = batch_logprob.gather(1, Variable(action_buffer[episode, :].long().view(-1, 1)))
+        selected_logprob = batch_logprob.gather(1, Variable(action_buffer[episode, :].long().view(-1, 1))) # log \pi(a | s)
+        # selected_logprob is $H$ length vector with [log pi(a0|s0), log pi (a1|s1), .., log pi (a_{h-1}|s_{h-1}}]
     else :
         _, batch_logprob = agent_.get_action(batch_input)
         selected_logprob = batch_logprob.gather(1, action_buffer[episode, :].long().view(-1, 1))
-    # this could be problem!! #
     loss = - torch.sum(selected_logprob * scores)
-    ##########################
     loss.backward()
     d = get_flat_grads_from(agent_)
 
@@ -155,10 +153,11 @@ def calculate_w(obs_buffer, action_buffer, episode,previous_policy, policy, num_
     selected_previous_logprob = previous_logprob.gather(1, action_buffer[episode, :].long().view(-1, 1))
     selected_logprob = current_logprob.gather(1, action_buffer[episode, :].long().view(-1, 1))
 
+    selected_previous_prob = torch.exp(selected_previous_logprob)
+    selected_prob = torch.exp(selected_logprob)
     epsilon = 1e-8
 
-    ratio = selected_previous_logprob / (selected_logprob +epsilon)
+    #donghao : nice catch! #
+    ratio = selected_previous_prob / (selected_prob + epsilon)
     ratio = ratio.detach()
     return torch.prod(ratio)
-    # log_ratio = torch.log(selected_previous_logprob + epsilon) - torch.log(selected_logprob + epsilon)
-    # return torch.exp(torch.sum(log_ratio))